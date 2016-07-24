8f7f9572f878fe9ce787f803f97a72f6e53e8430
skeerti2 <skeerti2@gmail.com>
2016-07-24 15:39:06 +0800

    Added implementation
    Added implementation to the makeCacheMatrix and cacheSolve functions.
    Tested the functions using a 2x2 matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## I have created a function makeCacheMatrix that contains a variable to cache the inverse. It returns a list object that contains
+## four functions namely,
+## set() : to set the matrix. It has input argument that is the matrix.
+## get() : It returns the matrix
+## setinv() : It has an input argument as a matrix (inverse calculated). It sets the inverse to the variable inv (for caching)
+## getinv() : It returns the cached inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+		
+	get <- function() {x}
 
+	setinv <- function(inverse) {
+		inv <<- inverse	
+	}
+
+	getinv <- function() { inv }
+
+	list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function takes the object returned by makeCacheMatrix as input argument. It retrieves the cached inverse an checks if it is not null, in which case, the inverse 
+## had already been calculated. The function then returns the cached inverse matrix and also prints "getting cached data".
+## Else, the inverse is calculated using solve() in R and set to the cache using setinv() function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+
+	inverseMatrix <- x$getinv()
+	
+	if(!is.null(inverseMatrix)) {
+		print("getting cached data")
+		return(inverseMatrix)
+	}	
+		
+	matrixData <- x$get()
+	
+	inverseMatrix <- solve(matrixData)
+	
+	x$setinv(inverseMatrix)
+	
+	inverseMatrix
 }
